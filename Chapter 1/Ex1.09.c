fa973258caa681daa4d807be3a378e78445eecff
andrea-come <60099460+andrea-come@users.noreply.github.com>
2025-07-13 08:46:51 +0200

    Create Ex1.09.c
    First version of exercise 9

---
+++ Chapter 1/Ex1.09.c
@@ -0,0 +1,28 @@
+/* Ex 1.9 from "The C Programming Language" by Kernighan & Ritchie
+   
+   Write a program to copy its input to its output, replacing each
+   string of one or more blanks by a single blank
+
+   2025 07 13
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+  int c,      // input character
+      nb = 0; // number of blanks
+
+  while( (c = getchar()) != EOF ) {
+    if(c == ' '){
+      nb++;
+      if(nb < 2) { putchar(c); }
+    }        
+    else {
+      putchar(c);
+      nb = 0; 
+    }       
+  }
+   
+  return 0;
+}
